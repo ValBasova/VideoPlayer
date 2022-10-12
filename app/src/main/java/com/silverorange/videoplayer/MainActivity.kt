f780295d1094e144de6efc385fb19755de6bf907
Valery Basova <basovavalery@gmail.com>
2022-10-12 11:25:45 -0400

    enable DataBinding

--- app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.silverorange.videoplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.silverorange.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
