11a795cf935aa88419f7047360d077227d89610b
Valery Basova <basovavalery@gmail.com>
2022-10-12 13:40:09 -0400

    initializer exoplayer

--- app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.util.Util
 import com.silverorange.videoplayer.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var videoList: List<Video>
+
+    private var player: ExoPlayer? = null
 
     private var job = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + job)
@@ -28,17 +35,76 @@ class MainActivity : AppCompatActivity() {
         uiScope.launch {
             fetchVideos()
         }
+
+        initializePlayer()
     }
 
     private suspend fun fetchVideos() {
         try {
             videoList = VideoApi.retrofitMoshiService.getAllVideos()
-
+            initializePlayer()
             binding.video = videoList[0]
             binding.invalidateAll()
         } catch (e: Exception) {
             Log.e("GetVideos API error", e.message.toString())
             videoList = listOf()
         }
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(applicationContext).build()
+        binding.playerView.player = player
+
+        videoList.let {
+            if (videoList.isNotEmpty()) {
+                val mediaItem = MediaItem.fromUri(videoList.get(0).url)
+                player?.addMediaItem(mediaItem)
+            }
+        }
+        player?.prepare()
+        player?.play()
+
+        player?.addListener(object : Player.Listener {
+            override fun onPlayerError(error: PlaybackException) {
+                super.onPlayerError(error)
+                Log.e("Player error", error.message.toString())
+            }
+        })
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.let { exoPlayer ->
+            exoPlayer.release()
+        }
+        player = null
     }
 }
--- app/src/main/java/com/silverorange/videoplayer/Video.kt
+++ app/src/main/java/com/silverorange/videoplayer/Video.kt
@@ -1,5 +1,12 @@
 package com.silverorange.videoplayer
 
+import com.squareup.moshi.Json
+
 data class Video(
-    val id: String
+    val id: String,
+    @Json(name = "fullURL")
+    val url: String,
+    val title: String,
+//    val author: String,
+    val description: String
 )
